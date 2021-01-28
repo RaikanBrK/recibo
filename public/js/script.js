a6cd267b135d4a8d230abad69500253b2c611cbb
Carlos <carlos24106@outlook.com>
2021-01-28 17:02:09 -0300

    Melhorando sistema para ativar a animação do hamburguer

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
 	$('#userContentHeader').click(() => {
 		$('.content-suport-user').slideToggle('slow');
+
+		$('.icon-down').toggleClass('icon-select');
 	});
 
 	$(window).click(function(e) {
@@ -14,11 +16,21 @@ $(document).ready(() => {
 			(element.attr('id') != "userContentHeader" && element.closest("#userContentHeader").length == 0)
 		) {
 			$('.content-suport-user').slideUp('slow');
+			$('.icon-down').removeClass('icon-select');
 		}
 	});
 
 	$('#hamburguer').click(function() {
-		$(this).toggleClass('burgerActive');
+		let burguer = $('#hamburguer > .hamburguer');
+		burguer.toggleClass('burguerActive');
+
+		if (burguer.hasClass('burguerActive') && $('#menu').hasClass('show') ) {
+
+			burguer.removeClass('burguerActive')
+		} else if(burguer.hasClass('burguerActive') == false && $('#menu').hasClass('show') == false) {
+
+			burguer.addClass('burguerActive');
+		}
 	});
 
 });
