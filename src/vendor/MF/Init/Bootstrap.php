f4f73821254d5f5bc905c540c810d752e659bb24
Carlos Alexandre <carlos24106@outlook.com>
2021-02-07 17:28:29 -0300

    Melhorei sistema de rotas - Com rotas dinâmicas e grupos de rotas

--- src/vendor/MF/Init/Bootstrap.php
+++ src/vendor/MF/Init/Bootstrap.php
@@ -17,18 +17,55 @@ abstract class Bootstrap {
 	}
 
 	public function setRoutes(array $routes) {
-		$this->routes = $routes;
+		$routesEdit = [];
+		foreach ($routes as $key => $route) {
+			if (isset($route['group'])) {
+				$route['route'] = $route['group'] . $route['route'];
+			}
+
+			$routesEdit[$key] = $route;
+		}
+
+		$this->routes = $routesEdit;
 	}
 
 	protected function run($url) {
 		foreach ($this->getRoutes() as $key => $route) {
-			if ($url == $route['route']) {
-				$class = "App\\Controllers\\".ucfirst($route['controller']);
-				$controller = new $class;
+			$validRoute = false;
 
-				$action = $route['action'];
-				$controller->$action();
+			if ($url == $route['route']) {
+				$validRoute = true;
+			} else {
+
+				/**
+				 * Rotas Dinâmicas
+				 */
+				$arrayUrl = explode('/', $url);
+				unset($arrayUrl[count($arrayUrl) - 1]);
+				$stringUrl = implode('/', $arrayUrl);
+
+				$newUrl = $stringUrl . '/{}';
+
+				if ($newUrl == $route['route']) {
+					$validRoute = true;
+				}
 			}
+
+			if ($validRoute) {
+				if (isset($route['group'])) {
+					$this->startControllerAction($route['controller'], str_replace('/', '', $route['group']));
+				}
+				$this->startControllerAction($route['controller'], $route['action']);
+			}
+		}
+	}
+
+	private function startControllerAction($controller, $action) {
+		$class = "App\\Controllers\\".ucfirst($controller);
+		$controller = new $class;
+
+		if (method_exists($controller, $action)) {
+			$controller->$action();
 		}
 	}
 
