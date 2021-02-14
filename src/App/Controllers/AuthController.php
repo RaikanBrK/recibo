99cecbdb28adb1de220e41e0dac20046349dbec1
Carlos Alexandre <carlos24106@outlook.com>
2021-02-14 19:31:20 -0300

    Recebendo dados para cadastrar usuário no banco de dados

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -12,77 +12,95 @@ class AuthController extends Action {
 		$this->view->js = ['auth'];
 		$this->view->title = 'Cadastrar usuário';
 
-		$error = filter_input(INPUT_GET, "error", FILTER_SANITIZE_STRING);
-		$code = filter_input(INPUT_GET, "code", FILTER_SANITIZE_STRING);
+		$facebook = new Facebook(FACEBOOK);
+		$this->view->authUrlFacebook = $facebook->getAuthorizationUrl([
+			"scope" => ["email"]
+		]);
 
-		/**
-		 * Auth Google
-		 */
-		// $google = new Google(GOOGLE);
-		// $this->view->authUrl = $google->getAuthorizationUrl();
-
-		// if ($error) {
-		// 	// Tratar erro
-		// }
-
-		// if ($code) {
-		// 	$token = $google->getAccessToken("authorization_code", [
-		// 		"code" => $code
-		// 	]);
-
-		// 	$user = unserialize(serialize($google->getResourceOwner($token)));
-		// 	$infoUser = $user->toArray();
-
-		// 	echo '<pre>';
-		// 		print_r($user);
-		// 	echo '</pre>';
-
-		// 	echo '<pre>';
-		// 		print_r($infoUser);
-		// 	echo '</pre>';
-		// }
-
-		/**
-		 * Auth Facebook
-		 */
-		// $facebook = new Facebook(FACEBOOK);
-		// $this->view->authUrlFacebook = $facebook->getAuthorizationUrl([
-		// 	"scope" => ["email"]
-		// ]);
-
-		// if ($error) {
-		// 	// Tratar erro
-		// }
-
-		// if ($code) {
-		// 	$token = $facebook->getAccessToken("authorization_code", [
-		// 		"code" => $code
-		// 	]);
-
-		// 	$user = unserialize(serialize($facebook->getResourceOwner($token)));
-		// 	$infoUser = $user->toArray();
-
-		// 	echo '<pre>';
-		// 		print_r($user);
-		// 	echo '</pre>';
-
-		// 	echo '<pre>';
-		// 		print_r($infoUser);
-		// 	echo '</pre>';
-		// }
+		$google = new Google(GOOGLE);
+		$this->view->authUrlGoogle = $google->getAuthorizationUrl();
 
 		$this->render('cadastro', 'layoutAuth');
 	}
 
 	public function cadastrarUsuario() {
 		$regras = new RegrasCadastroUsuario();
-		$valid = $regras->validarAll($_POST);
 
-		var_dump($valid);
+		$paramFacebook = filter_input(INPUT_GET, 'facebook', FILTER_SANITIZE_STRING);
+		$paramGoogle = filter_input(INPUT_GET, 'google', FILTER_SANITIZE_STRING);
 
-		echo '<pre>';
-			print_r($regras->__get('fail'));
-		echo '</pre>';
+		$paramError = filter_input(INPUT_GET, 'error', FILTER_SANITIZE_STRING);
+		$paramCode = filter_input(INPUT_GET, 'code', FILTER_SANITIZE_STRING);
+
+		if ($paramFacebook) {
+			/**
+			 * Auth Facebook
+			 */
+			$facebook = new Facebook(FACEBOOK);
+
+			if ($paramError) {
+				// Tratar erro
+			}
+
+			if ($paramCode) {
+				$token = $facebook->getAccessToken("authorization_code", [
+					"code" => $paramCode
+				]);
+
+				$user = unserialize(serialize($facebook->getResourceOwner($token)));
+
+				echo '<pre>';
+					print_r($user->getFirstName());
+				echo '</pre>';
+
+				echo '<pre>';
+					print_r($user->getLastName());
+				echo '</pre>';
+
+				echo '<pre>';
+					print_r($user->getEmail());
+				echo '</pre>';
+			}
+
+
+		} else if ($paramGoogle) {
+			/**
+			 * Auth Google
+			 */
+			$google = new Google(GOOGLE);
+
+			if ($paramError) {
+				// Tratar erro
+			}
+
+			if ($paramCode) {
+				$token = $google->getAccessToken("authorization_code", [
+					"code" => $paramCode
+				]);
+
+				$user = unserialize(serialize($google->getResourceOwner($token)));
+
+				echo '<pre>';
+					print_r($user->getFirstName());
+				echo '</pre>';
+
+				echo '<pre>';
+					print_r($user->getLastName());
+				echo '</pre>';
+
+				echo '<pre>';
+					print_r($user->getEmail());
+				echo '</pre>';
+			}
+		} else {
+			# Login Email
+			## nome(sobrenome), email, senha e confirmarSenha
+			$valid = $regras->validarAll($_POST);
+
+			echo '<pre>';
+				print_r($regras->__get('fail'));
+			echo '</pre>';
+		}
 	}
 
 	public function login() {
