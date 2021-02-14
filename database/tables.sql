13431fd9afec010bde2aa954aa943c62aa8233e7
Carlos Alexandre <carlos24106@outlook.com>
2021-02-14 19:32:22 -0300

    Criando tabelas de usuários no banco de dados

---
+++ database/tables.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS `receipt` DEFAULT CHARACTER SET utf8;
+use receipt;
+
+CREATE TABLE IF NOT EXISTS `receipt`.`auth_social` (
+	id INT NOT NULL AUTO_INCREMENT,
+	social CHAR(30) NOT NULL,
+
+	PRIMARY KEY(`id`)
+) ENGINE = InnoDB;
+
+REPLACE INTO `receipt`.`auth_social` (id, social) VALUES (1, 'Email'),
+(2, 'Facebook'),
+(3, 'Google');
+
+CREATE TABLE IF NOT EXISTS `receipt`.`usuarios` (
+	id INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(100) NOT NULL,
+	email VARCHAR(200) NOT NULL UNIQUE,
+	senha VARCHAR(50) NULL,
+
+	authSocialId INT NULL DEFAULT 1,
+	FOREIGN KEY(authSocialId) REFERENCES auth_social(id),
+
+	PRIMARY KEY(`id`)
+) ENGINE = InnoDB;
