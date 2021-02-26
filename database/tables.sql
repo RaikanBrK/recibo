e2dd0c62c9bfa57464641105d445e5564e4cd6a1
Carlos Alexandre <carlos24106@outlook.com>
2021-02-26 09:41:07 -0300

    Criando uma coluna img para as imagens do usuário

--- database/tables.sql
+++ database/tables.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS `receipt`.`usuarios` (
 	nome VARCHAR(40) NOT NULL,
 	email VARCHAR(100) NOT NULL UNIQUE,
 	senha VARCHAR(150) NULL,
+	img VARCHAR(300) NULL,
 
 	authSocialId INT NULL DEFAULT 1,
 	FOREIGN KEY(authSocialId) REFERENCES auth_social(id),
